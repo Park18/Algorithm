47107d9ce974d4471a855b9ab513f6df14a5fd5e
Park18 <sowooni35@naver.com>
2020-05-07 22:15:54 +0900

    add 11726 comment

--- 11726.hpp
+++ 11726.hpp
@@ -1,3 +1,10 @@
+/*****************************************************************
+ * https://www.acmicpc.net/problem/11726
+ * 문제
+ * 2xn 크기의 직사각형을 1x2, 2x1 타일로 채우는 방법의 수를 구하는
+ * 프로그램을 작성하시오.
+ *****************************************************************/
+
 #pragma once
 #include <iostream>
 
@@ -5,36 +12,49 @@ using namespace std;
 
 namespace my
 {
-int fibo(int n)
-{
-	if (n == 0 || n == 1) return 1;
-	return fibo(n - 1) + fibo(n - 2);
-}
+	/**
+	 * @brief 피보나치 함수
+	 * @param int n 
+	 */
+	int fibo(int n)
+	{
+		if (n == 0 || n == 1) return 1;
+		return fibo(n - 1) + fibo(n - 2);
+	}
 
-void test_main_1(int argc, char* argv[])
-{
-	int n;
-	cin >> n;
+	/**
+	 * @brief 피보나치를 이용한 문제풀이
+	 */
+	void test_main_1(int argc, char* argv[])
+	{
+		int n;
+		cin >> n;
 
-	int count = fibo(n);
-	cout << count % 10007 << endl;
-}
+		int count = fibo(n);
+		cout << count % 10007 << endl;
+	}
 
-void test_main_2()
-{
-	int count[1001];
-	count[1] = 1;
-	count[2] = 2;
+	/**
+	 * @brief DP를 이용한 문제풀이
+	 */
+	void test_main_2()
+	{
+		int count[1001];	// 1<=n<=1000
+		count[1] = 1;		// 초기값1
+		count[2] = 2;		// 초기값2
 
-	int n;
-	cin.tie(nullptr);
-	cin >> n;
+		int n;
+		cin.tie(nullptr);
+		cin >> n;
 
-	for (int index = 3; index <= n; index++)
-		count[index] = (count[index - 1] + count[index - 2]) % 10007;
+		// DP 계산
+		// 그냥 피보나치로만 계산하면 값이 매우 커지기 때문에
+		// 10007을 값으로 초기화한다.
+		for (int index = 3; index <= n; index++)
+			count[index] = (count[index - 1] + count[index - 2]) % 10007;
 
-	cout << count[n] << endl;
+		cout << count[n] << endl;
 	
-}
+	}
 
 }
