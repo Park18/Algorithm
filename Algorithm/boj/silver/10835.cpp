26ec0816cabc3c2c9599b569c25f5d8470de96b3
Park18 <sowoomi35@gmail.com>
2020-09-14 20:54:45 +0900

    UPDATE 10835

--- Algorithm/boj/silver/10835.cpp
+++ Algorithm/boj/silver/10835.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int N;
-vector<int> left;
-vector<int> right;
+vector<int> left_dummy;
+vector<int> right_dummy;
 
 void input_data()
 {
@@ -16,33 +16,52 @@ void input_data()
 	for (int index = 0; index < N; index++)
 	{
 		cin >> data;
-		::left.push_back(data);
+		left_dummy.push_back(data);
 	}
 
 	for (int index = 0; index < N; index++)
 	{
 		cin >> data;
-		::right.push_back(data);
+		right_dummy.push_back(data);
 	}
 }
 
-void sort()
+void dummy_sort()
 {
-	std::sort(::left.begin(), ::left.end());
-	std::sort(::right.begin(), ::right.end());
+	std::sort(left_dummy.begin(), left_dummy.end(), greater<int>());
+	std::sort(right_dummy.begin(), right_dummy.end(), greater<int>());
 }
 
-void solution()
+int solution()
 {
-	
+	int result = 0;
+	int left_index = 0;
+	int right_index = 0;
+
+	while (1)
+	{
+		if (left_dummy[left_index] > right_dummy[right_index])
+		{
+			result = max(result, result + right_dummy[right_index]);
+			left_index++;
+			right_index++;
+		}
+
+		else
+			right_index++;
+
+		if ((N <= left_index) || (N <= right_index))
+			break;
+	}
+
+	return result;
 }
 
 int main()
 {
 	input_data();
 
-	for (auto i : ::left)
-		cout << i << endl;
-	for (auto i : ::right)
-		cout << i << endl;
+	dummy_sort();
+
+	cout << solution() << endl;
 }
