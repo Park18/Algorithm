eb71fe5e208b90c76a76f1576b4a0e7fd30dfe2f
Park18 <sowoomi35@gmail.com>
2020-09-27 21:32:24 +0900

    UPDATE solution

---
+++ Algorithm/boj/gold/14621.cpp
@@ -0,0 +1,19 @@
+/**
+ * @date 20.09.26
+ * @url https://www.acmicpc.net/problem/14621
+ */
+
+#include<iostream>
+
+using namespace std;
+
+#define MALE true
+#define FEMALE false
+
+struct NODE
+{
+	bool sex;
+
+	NODE(bool sex) :sex(sex) {};
+};
+
