9a75918ee3c2262c5e5eb601894a9b0500db8700
Park18 <sowoomi35@gmail.com>
2020-10-26 21:06:27 +0900

    CREATE 7453

---
+++ Algorithm/boj/gold/7453.cpp
@@ -0,0 +1,36 @@
+/**
+ * @date 20.10.24
+ * @url
+ * @result
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define ARRAY vector<int> 
+
+int N;
+int count;
+ARRAY A;
+ARRAY B;
+ARRAY C;
+ARRAY D;
+
+void input_data()
+{
+	cin >> N;
+
+
+}
+
+void solved()
+{
+
+}
+
+int main()
+{
+
+}
