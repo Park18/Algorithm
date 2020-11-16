b78ea2462da77f6e4679a7933f561723ed389564
Park18 <sowoomi35@gmail.com>
2020-11-16 20:23:00 +0900

    CREATE platinum directory and 2873

---
+++ Algorithm/boj/platinum/2873.cpp
@@ -0,0 +1,18 @@
+/**
+ * @date 20.11.11
+ * @url https://www.acmicpc.net/problem/2873
+ * @result 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int R, C;
+
+
+void input_data()
+{
+	cin >> R >> C;
+
+}
