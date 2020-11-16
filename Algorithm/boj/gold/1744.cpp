15a2f0201f05b4d08e9d7f128be97e6e433d92a2
Park18 <sowoomi35@gmail.com>
2020-11-16 20:24:53 +0900

    UPDATE 1744

--- Algorithm/boj/gold/1744.cpp
+++ Algorithm/boj/gold/1744.cpp
@@ -1,11 +1,12 @@
 /**
  * @date 20.10.31
  * @url https://www.acmicpc.net/problem/1744
- * @reuslt 실패
+ * @reuslt 성공
  */
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 #define MAX_SIZE 10000
 #define MIN_VALUE -100000000
@@ -13,31 +14,61 @@
 using namespace std;
 
 int N;
-int sequence[MAX_SIZE + 1];
-int dp[MAX_SIZE + 1];
-int result = MIN_VALUE;
+vector<int> plus_group;
+vector<int> minus_group;
+int result = 0;
 
 void input_data()
 {
 	cin >> N;
 	for (int index = 1; index <= N; index++)
-		cin >> sequence[index];
+	{
+		int num;
+		cin >> num;
+		if (num > 0)
+			plus_group.push_back(num);
+		else
+			minus_group.push_back(num);
+	}
 }
 
 void solved()
 {
-	sort(sequence + 1, sequence + (N + 1));
+	sort(plus_group.begin(), plus_group.end(), greater<int>());
+	sort(minus_group.begin(), minus_group.end(), less<int>());
 
-	result = dp[1] = sequence[1];
-	for (int index = 2; index <= N; index++)
-		result = dp[index] = max(result, max(dp[index - 2] + sequence[index - 1] + sequence[index], dp[index - 2] + (sequence[index - 1] * sequence[index])));
+	for (int index = 0; index < plus_group.size(); index++)
+	{
+		if (index == plus_group.size() - 1)
+		{
+			result += plus_group[index];
+			break;
+		}
+
+		int sum = plus_group[index] + plus_group[index + 1];
+		int mul = plus_group[index] * plus_group[index + 1];
+		sum > mul ? result += plus_group[index] : (result += mul, index++);
+	}
+
+	for (int index = 0; index < minus_group.size(); index++)
+	{
+		if (index == minus_group.size() - 1)
+		{
+			result += minus_group[index];
+			break;
+		}
+
+		int sum = minus_group[index] + minus_group[index + 1];
+		int mul = minus_group[index] * minus_group[index + 1];
+		sum > mul ? result += minus_group[index] : (result += mul, index++);
+	}
 }
 
 int main()
 {
 	input_data();
-
+	
 	solved();
-
+	
 	cout << result << endl;
 }
