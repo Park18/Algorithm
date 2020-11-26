966463ad29ae86f297ffb5ebdad34d6bd7f98303
Park18 <sowoomi35@gmail.com>
2020-11-26 14:20:22 +0900

    UPDATE 3665

--- boj/gold/3665.cpp
+++ boj/gold/3665.cpp
@@ -1,60 +1,67 @@
 /**
  * @date 20.11.23
- * @url https://www.acmicpc.net/problem/3665
- * @result 
- * @source https://deque.tistory.com/62
+ * @url	https://www.acmicpc.net/problem/3665
+ * @result	
+ * @source	https://deque.tistory.com/62
+ *			https://js1jj2sk3.tistory.com/101
  */
 
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 
 #define UNKOWN "?"
 #define IMPOSSIBLE "IMPOSSIBLE"
 #define MAX_SIZE 500
 #define endl '\n'
+
 using namespace std;
 
 int T, n, m;
 int in_degree[MAX_SIZE + 1];			// 진입차수
-int graph[MAX_SIZE + 1][MAX_SIZE + 1];	// 위상정렬을 위한 그래프
+bool graph[MAX_SIZE + 1][MAX_SIZE + 1];	// 위상정렬을 위한 그래프
 int ranking[MAX_SIZE + 1];				// 순위
 vector<int> result;
 bool is_unkown;
 
 void input_data()
 {
+	memset(in_degree, 0, sizeof(in_degree));
+	memset(graph, false, sizeof(graph));
+	memset(ranking, 0, sizeof(ranking));
+	result.clear();
+
 	cin >> n;
 	for (int index = 1; index <= n; index++)
 		cin >> ranking[index];
 
 	for (int i = 1; i <= n; i++)
 	{
-		for (int j = i + 1; j <= j; j++)
+		for (int j = i + 1; j <= n; j++)
 		{
-			graph[ranking[i]][ranking[j]] = 1;
+			graph[ranking[i]][ranking[j]] = true;
 			in_degree[ranking[j]]++;
 		}
 	}
 
 	cin >> m;
-	for (int index = 0; index < m; index++)
+	for (int loop = 0; loop < m; loop++)
 	{
 		int a, b;
 		cin >> a >> b;
 		if (graph[a][b])
 		{
-			graph[a][b] = 0;
-			graph[b][a] = 1;
+			graph[a][b] = false;
+			graph[b][a] = true;
 			in_degree[a]++, in_degree[b]--;
 		}
 		else
 		{
-			graph[a][b] = 1;
-			graph[b][a] = 0;
+			graph[a][b] = true;
+			graph[b][a] = false;
 			in_degree[a]--, in_degree[b]++;
 		}
-
 	}
 }
 
@@ -97,8 +104,11 @@ void print()
 		cout << UNKOWN << endl;
 
 	else if (result.size() == n)
+	{
 		for (auto team : result)
-			cout << team << endl;
+			cout << team << ' ';
+		cout << endl;
+	}
 
 	else
 		cout << IMPOSSIBLE << endl;
@@ -107,15 +117,13 @@ void print()
 int main()
 {
 	cin >> T;
-
+	
 	for (int loop = 0; loop < T; loop++)
 	{
 		input_data();
-
+	
 		solved();
-
+	
 		print();
-
-		result.clear();
 	}
 }
